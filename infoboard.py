204f34d912203a492016ca53454de9c6d6446efa
Nathaniel Case <Qalthos@gmail.com>
2012-11-29 18:08:37 -0500

    Use knowledge for caching.

--- infoboard.py
+++ infoboard.py
@@ -11,7 +11,13 @@ from github import Github
 
 ORG = "FOSSRIT"
 
+# Setup caching
 base_dir = os.path.split(__file__)[0]
+from sqlalchemy import create_engine
+from knowledge.model import init_model, metadata, DBSession, Entity
+engine = create_engine('sqlite://{0}/knowledge.db'.format(base_dir))
+init_model(engine)
+metadata.create_all(engine)
 
 
 class InfoWin(Gtk.Window):
@@ -24,31 +30,32 @@ class InfoWin(Gtk.Window):
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         latest_events = self.org.get_events()[:10]
         for event in latest_events:
-            box.add(Spotlight(event))
+            box.add(Spotlight(event_info(event)))
         self.add(box)
 
 
 class Spotlight(Gtk.Box):
     def __init__(self, event):
         super(Spotlight, self).__init__()
-        user = event.actor
-        self.add(url_to_image(user.avatar_url, user.gravatar_id))
+        user = event.children.get(event[u'actor'])
+        self.add(url_to_image(user[u'avatar'], user[u'gravatar']))
         event_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        if event.type == "CreateEvent":
+        if event[u'type'] == "CreateEvent":
             event_box.add(Gtk.Label("{0} created a new {1}."
-                .format(user.name, event.payload['ref_type'])))
-            event_box.add(Gtk.Label(event.payload['description']))
-        elif event.type == "IssueCommentEvent":
+                .format(user[u'name'], event[u'payload']['ref_type'])))
+            event_box.add(Gtk.Label(event[u'payload']['description']))
+        elif event[u'type'] == "IssueCommentEvent":
             event_box.add(Gtk.Label("{0} commented on an issue."
-                .format(user.name)))
-            event_box.add(Gtk.Label(event.payload['comment']['body']))
-        elif event.type == "PushEvent":
+                .format(user[u'name'])))
+            comment = event.children.get(event[u'comment'])
+            event_box.add(Gtk.Label(comment[u'body']))
+        elif event[u'type'] == "PushEvent":
             event_box.add(Gtk.Label("{0} pushed {1} commits."
-                .format(user.name, len(event.payload['commits']))))
-            for commit in event.payload['commits']:
+                .format(user[u'name'], len(event[u'payload']['commits']))))
+            for commit in event[u'payload']['commits']:
                 event_box.add(Gtk.Label(commit['message']))
         else:
-            event_box.add(Gtk.Label(event.type))
+            event_box.add(Gtk.Label(event['type']))
         self.add(event_box)
 
 
@@ -58,6 +65,44 @@ def url_to_image(url, filename):
         urlretrieve(url, local_path)
     img = Gtk.Image.new_from_file(local_path)
     return img
+
+
+def event_info(event):
+    event_name = u'event_{0}'.format(event.id)
+    if not Entity.by_name(event_name):
+        entity = Entity(event_name)
+        actor = user_info(event.actor)
+        entity[u'actor'] = actor.name
+        entity.append(actor)
+        entity[u'repo'] = event.repo.name
+        entity[u'type'] = event.type
+        entity[u'payload'] = event.payload
+        if event.type == "IssueCommentEvent":
+            comment = comment_info(event.payload['comment'])
+            entity.append(comment)
+            entity[u'comment'] = comment.name
+        DBSession.add(entity)
+    return Entity.by_name(event_name)
+
+
+def user_info(user):
+    user_name = u'user_{0}'.format(user.id)
+    if not Entity.by_name(user_name):
+        entity = Entity(user_name)
+        entity[u'avatar'] = user.avatar_url
+        entity[u'gravatar'] = user.gravatar_id
+        entity[u'name'] = user.name
+        DBSession.add(entity)
+    return Entity.by_name(user_name)
+
+
+def comment_info(comment):
+    comment_name = u'comment_{0}'.format(comment['id'])
+    if not Entity.by_name(comment_name):
+        entity = Entity(comment_name)
+        entity[u'body'] = comment['body']
+        DBSession.add(entity)
+    return Entity.by_name(comment_name)
 
 
 if __name__ == "__main__":
