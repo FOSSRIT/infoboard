7c05ef0d2f1a439e10953188374118d3e81a3638
Nathaniel Case <Qalthos@gmail.com>
2012-12-01 16:24:04 -0500

    Add setup.py :D

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='infoboard',
+    version='1.0',
+    description='An app for tracking user contributions to Github organizations',
+    author='Nathaniel Case',
+    author_email='Qalthos@gmail.com',
+    url='https://github.com/FOSSRIT/infoboard',
+    packages=find_packages(),
+    install_requires=['pygithub', 'knowledge']
+)
