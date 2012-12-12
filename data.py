822fa206dda623218279e95243cc81658b3ecb79
Nathaniel Case <Qalthos@gmail.com>
2012-12-12 17:11:27 -0500

    Increase total count for number of commits as well as commit score.

--- data.py
+++ data.py
@@ -23,11 +23,15 @@ def top_contributions():
     user_activity = defaultdict(lambda: defaultdict(int))
     repo_activity = defaultdict(lambda: defaultdict(int))
     for event in week_activity:
-        user_activity[event['actor']]['count'] += 1
         if event['type'] == 'PushEvent':
-            user_activity[event['actor']]['commits'] += event['payload']['size']
+            changes = event['payload']['size']
+            key = 'commits'
         else:
-            user_activity[event['actor']][event['type']] += 1
+            changes = 1
+            key = event['type']
+        user_activity[event['actor']]['count'] += changes
+        user_activity[event['actor']][key] += changes
+
         if len(event['repo'].split('/')) == 1:
             if event['type'] == 'PushEvent':
                 changes = event['payload']['size']
